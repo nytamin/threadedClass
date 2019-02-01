96589b228674bca49f999f2618f9ad192ac7c124
superflytvab <info@superfly.tv>
2019-02-01 16:14:30 +0000

    Updates

---
+++ examples/myClass.js
@@ -0,0 +1,15 @@
+class MyClass {
+    doSomethingSlow () {
+        this._fib(37)
+    }
+    /** Calculate fibbonacci number, takes a long time */
+    _fib (num) {
+        let result = 0
+        if (num < 2) {
+            result = num
+        } else {
+            result = this._fib(num - 1) + this._fib(num - 2)
+        }
+        return result
+    }
+}
