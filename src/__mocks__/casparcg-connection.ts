57cfa6e8302c58a55907d719ad200a1951333064
Johan Nyman <johan@nytamin.se>
2020-10-26 08:20:15 +0100

    chore: add test which is using the jest mock

---
+++ src/__mocks__/casparcg-connection.ts
@@ -0,0 +1,8 @@
+
+export class CasparCG {
+    constructor() {
+    }
+    isMock() {
+        return true
+    }
+}
---
+++ src/__tests__/test-mock.spec.ts
@@ -0,0 +1,29 @@
+jest.mock('casparcg-connection')
+import { threadedClass, ThreadedClassManager } from '..'
+import { TestClass2 } from '../../test-lib/testClass2'
+const TESTCLASS_PATH = '../../test-lib/testClass2.js'
+
+describe('Test with Jest mock', () => {
+    test('mock', async () => {
+
+        // This test is making sure jest is mocking modules that are used inside the threaded class.
+
+        for (const disableMultithreading of [true, false]) {
+
+            let original = new TestClass2()
+            expect(original.isOkay()).toEqual(true)
+
+            let threaded = await threadedClass<TestClass2, typeof TestClass2>(TESTCLASS_PATH, 'TestClass2', [], { disableMultithreading })
+            let onClosed = jest.fn()
+            ThreadedClassManager.onEvent(threaded, 'thread_closed', onClosed)
+
+            expect(await threaded.isOkay()).toEqual(true)
+
+            await ThreadedClassManager.destroy(threaded)
+            expect(ThreadedClassManager.getThreadCount()).toEqual(0)
+
+            expect(onClosed).toHaveBeenCalledTimes(1)
+        }
+
+    })
+})
