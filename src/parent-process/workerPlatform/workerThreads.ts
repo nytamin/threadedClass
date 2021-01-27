e931372585770acfe960495726e6bbbb167e8d5c
Julian Waller <git@julusian.co.uk>
2021-01-27 14:27:45 +0000

    feat: add environemnt variable to use a worker_thread loader script (#42)
    One is used by default if threadedClass detects it is inside an electron asar file, to allow the worker_thread to read its source from inside the asar

--- src/parent-process/workerPlatform/workerThreads.ts
+++ src/parent-process/workerPlatform/workerThreads.ts
@@ -2,8 +2,13 @@ import { Worker as IWorker } from 'worker_threads'
 import { Message } from '../../shared/sharedApi'
 import { getWorkerThreads } from '../../shared/lib'
 import { WorkerPlatformBase } from './_base'
+import { readFileSync } from 'fs'
+import * as path from 'path'
 
 const WorkerThreads = getWorkerThreads()
+
+const DEFAULT_ELECTRON_LOADER = path.join(__dirname, '../../js/asar-loader.js')
+const isInAsar = Object.prototype.hasOwnProperty.call(process.versions, 'electron') && DEFAULT_ELECTRON_LOADER.match(/.asar(\/|\\)/)
 
 /** Functions for spawning worker-threads in NodeJS */
 
@@ -16,9 +21,29 @@ export class WorkerThread extends WorkerPlatformBase {
 		// @ts-ignore
 		// this.worker = new window.Worker(pathToWorker)
 		if (!WorkerThreads) throw new Error('Unable to create Worker thread! Not supported!')
-		this.worker = new WorkerThreads.Worker(pathToWorker, {
-			workerData: ''
-		})
+
+		// Figure out the loader to use. This is to allow for some environment setup (eg require behaviour modification) before trying to run threadedClass
+		let loader = process.env.THREADEDCLASS_WORKERTHREAD_LOADER
+		if (!loader && isInAsar) {
+			loader = DEFAULT_ELECTRON_LOADER
+		}
+
+		if (loader) {
+			// The WorkerThreads may will not be able to load this file, so we must do it in the parent
+			const buf = readFileSync(loader)
+
+			// Start the WorkerThread, passing pathToWorker so that the loader knows what it should execute
+			this.worker = new WorkerThreads.Worker(buf.toString(), {
+				workerData: pathToWorker,
+				eval: true
+			})
+		} else {
+			// No loader, so run the worker directly
+			this.worker = new WorkerThreads.Worker(pathToWorker, {
+				workerData: ''
+			})
+		}
+
 		this.worker.on('message', (message: any) => {
 			this.emit('message', message)
 			// if (message.type === 'message') {
