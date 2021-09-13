bd89dfc74fe408823f13bdfa46ac96989b2c4cf1
Johan Nyman <johan@nytamin.se>
2021-09-13 14:15:17 +0200

    chore: lower the coverageThreshold a bit

--- jest.config.js
+++ jest.config.js
@@ -26,10 +26,10 @@ module.exports = {
 	testEnvironment: 'node',
 	coverageThreshold: {
 		global: {
-			statements: 70,
-			branches: 60,
-			functions: 70,
-			lines: 70,
+			statements: 60,
+			branches: 50,
+			functions: 60,
+			lines: 60,
 		}
 	},
 	coverageDirectory: "./coverage/",
