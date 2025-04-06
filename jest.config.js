e94d4da96f9ae410049780d33afd90e3fa0610e8
Julian Waller <git@julusian.co.uk>
2025-04-06 14:43:52 +0200

    feat: remove manual electron asar loader
    This has been unnecesary since electron 17.3, which was EOL in december 2022

--- jest.config.js
+++ jest.config.js
@@ -22,7 +22,6 @@ module.exports = {
 	],
 	coveragePathIgnorePatterns: [
         "src/webWorkers.ts",
-        "src/asar-loader.ts",
         "src/parent-process/workerPlatform/webWorkers.ts",
         "src/child-process/threadedclass-worker.ts",
     ],
